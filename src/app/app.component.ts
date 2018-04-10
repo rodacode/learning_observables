0b3f77015b97be6bc655af57b3609fcef26d4e7b
volatildub <nicolas.hardmeier@yahoo.es>
2018-04-10 03:51:04 +0200

    few basic examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  observable$;
+  mySubject$;
+
+  ngOnInit(){
+
+    
+    this.mySubject$  = new Subject();
+    this.mySubject$.subscribe(x => console.log('first subcribe',x));
+    this.mySubject$.next(1);
+    this.mySubject$.next(2);
+    this.mySubject$.subscribe(x => console.log('second subcribe',x));
+    this.mySubject$.next(3);
+
+    /*
+    this.observable$ = Observable.create((observer)=> {
+      observer.next(1);
+      observer.next(2);
+      observer.next(3);
+      observer.complete();
+    });
+    this.observable$.subscribe(
+      value => console.log(value),
+      err => {},
+      () => console.log('this is the end')
+    );
+    */
+  }
+
+  ngOnDestroy(){
+    //this.observable$.unsubcribe();
+    this.mySubject$.unsubcribe();
+  }
 }
